ef57b903be4fc1e496d79bfef5f1da3f4f76d1bc
WendaZheng <dylan.d.zheng@gmail.com>
2021-04-09 10:24:38 +0200

    graphic updated

--- src/com/plane/MyGameFrame.java
+++ src/com/plane/MyGameFrame.java
@@ -1,13 +1,27 @@
 package com.plane;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  *  A test Plane Game for learning purpose
  *
  *
  */
+
+
+
 public class MyGameFrame extends JFrame {
+
+    @Override
+    public void paint(Graphics g) {
+
+        g.setColor(Color.pink);
+        g.fillRect(100,100,300,300);
+        //g.drawString("Start",200,200);
+    }
 
     /**
      * initialize the windows
@@ -17,6 +31,14 @@ public class MyGameFrame extends JFrame {
         this.setVisible(true);
         this.setSize(500,500);
         this.setLocation(300,300);
+
+        this.addWindowListener(new WindowAdapter(){
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 
     }
 
